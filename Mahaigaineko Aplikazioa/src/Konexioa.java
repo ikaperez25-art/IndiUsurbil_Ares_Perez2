2ef08be2989b505e92645e30454dce1f6bbab938
Ares <ikaperez25@lhusurbil.eus>
2026-02-24 12:36:20 +0100

    y4whfd

--- Mahaigaineko Aplikazioa/src/Konexioa.java
+++ Mahaigaineko Aplikazioa/src/Konexioa.java
@@ -6,6 +6,11 @@ public class Konexioa {
     private static final String PASAHITZA = "gabrielito10";
 
     public static Connection getKonexioa() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL driver ez da aurkitu! Jar fitxategia falta da.");
+        }
         return DriverManager.getConnection(URL, ERABILTZAILEA, PASAHITZA);
     }
 }
