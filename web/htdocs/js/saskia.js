beb6f39a986e92ae9f9be2822f149ba7c996b7ca
Ares <ikaperez25@lhusurbil.eus>
2026-02-24 09:06:32 +0100

    css hobetzen

--- web/htdocs/js/saskia.js
+++ web/htdocs/js/saskia.js
@@ -16,30 +16,30 @@ function crearTarjetasProductosCarrito() {
                     <h3 class="produktu-izenburua">${produktua.title}</h3>
                     <div class="produktu-oina">
                         <span class="produktu-prezioa">${produktua.price.toFixed(2)}€</span>
-                        <div>
-                            <button>-</button>
-                            <span class="kopurua-testua">${produktua.cantidad}</span>
-                            <button>+</button>
+                        <div class="kopurua-kontrola">
+                            <button class="minus">-</button>
+                            <span class="kopuru-testua">${produktua.cantidad}</span>
+                            <button class="plus">+</button>
                         </div>
                     </div>
                 </div>
             `;
             saskiEdukiontzia.appendChild(txartelBerria);
 
-            txartelBerria.getElementsByTagName("button")[0].addEventListener("click", (e) => {
+            txartelBerria.querySelector(".minus").addEventListener("click", () => {
                 restarAlCarrito(produktua);
                 crearTarjetasProductosCarrito();
                 actualizarTotales();
             });
 
-            txartelBerria.getElementsByTagName("button")[1].addEventListener("click", (e) => {
+            txartelBerria.querySelector(".plus").addEventListener("click", () => {
                 agregarAlCarrito(produktua);
                 crearTarjetasProductosCarrito();
                 actualizarTotales();
             });
         });
     } else {
-        saskiEdukiontzia.innerHTML = "<p>Saskia hutsik</p>";
+        saskiEdukiontzia.innerHTML = "<p class='mezua-hutsik'>Saskia hutsik daukazu</p>";
     }
     actualizarTotales();
     actualizarNumeroCarrito();
@@ -50,39 +50,34 @@ function actualizarTotales() {
     let kopurua = 0;
     let prezioa = 0;
 
-    if (produktuak && produktuak.length > 0) {
-        produktuak.forEach((produktua) => {
-            kopurua += produktua.cantidad;
-            prezioa += produktua.price * produktua.cantidad;
-        });
-    }
+    produktuak.forEach((p) => {
+        kopurua += p.cantidad;
+        prezioa += p.price * p.cantidad;
+    });
 
     const guztizkoenEdukiontzia = document.getElementById("guztizkoak");
     if (guztizkoenEdukiontzia) {
         guztizkoenEdukiontzia.innerHTML = `
-            <div>Unitateak guztira: ${kopurua}</div>
-            <div>Prezio totala: ${prezioa.toFixed(2)}€</div>
-            <button id="saskia-hustu">Saskia hustu</button>
-            <button id="erosi-botoia">Erosi</button>
+            <div>Produktuak guztira: ${kopurua}</div>
+            <div>Guztira: ${prezioa.toFixed(2)}€</div>
+            <div class="saski-botoiak">
+                <button id="saskia-hustu">Saskia hustu</button>
+                <button id="erosi-botoia">Erosi orain</button>
+            </div>
         `;
 
-        const btnHustu = document.getElementById("saskia-hustu");
-        if (btnHustu) {
-            btnHustu.addEventListener("click", () => {
-                localStorage.removeItem("saskia");
-                crearTarjetasProductosCarrito();
-                actualizarTotales();
-            });
-        }
-        const btnErosi = document.getElementById("erosi-botoia");
-        if (btnErosi) {
-            btnErosi.onclick = function () {
-                alert("Erosketa eginda!");
-                localStorage.removeItem("saskia");
-                crearTarjetasProductosCarrito();
-                actualizarTotales();
-            }
-        }
+        document.getElementById("saskia-hustu").addEventListener("click", () => {
+            localStorage.removeItem("saskia");
+            crearTarjetasProductosCarrito();
+            actualizarTotales();
+        });
+
+        document.getElementById("erosi-botoia").addEventListener("click", () => {
+            alert("Erosketa ondo burutu da! Eskerrik asko.");
+            localStorage.removeItem("saskia");
+            crearTarjetasProductosCarrito();
+            actualizarTotales();
+        });
     }
 }
 
